e32356466c55fa250748e482f1a64a82966661c4
Tristan Sloughter <t@crashfast.com>
2014-11-22 17:03:15 -0600

    update for rebar3

--- src/rebar_escript_plugin.erl
+++ src/rebar_escript_plugin.erl
@@ -15,20 +15,21 @@
 %%%
 %%% @doc
 %%% A `rebar' plugin to package applications or releases as executable escript
-%%% similar to `rebar escriptize' but without its limitations. The plugin
-%%% automatically executes as `post_compile' or `post_generate' hook.
-%%% To use it specify it in the `plugins' section of your project's
-%%% `rebar.config', e.g. `{plugins, [rebar_escript_plugin]}.'
-%%% Depending on your preference put the plugin somewhere into your code path
-%%% or simply include it as project dependency.
+%%% similar to `rebar escriptize' but without its limitations.
+%%% To use it specify it in the `plugins' section of your project's `rebar.config', e.g.
+%%% `{plugins, [{rebar_escript_plugin, "",
+%%%               {git, "https://github.com/tsloughter/rebar_escript_plugin.git",
+%%%                  {branch, "master"}}}]}.'
 %%% @end
 %%%=============================================================================
 -module(rebar_escript_plugin).
 
-%% API
--export([post_compile/2,
-         post_generate/2,
-         post_clean/2]).
+-behaviour(provider).
+
+-export([init/1, do/1, format_error/1, clean/2]).
+
+-define(PROVIDER, escriptize).
+-define(DEPS, [compile]).
 
 -define(TEMP_DIR, ".escript").
 -define(MAIN_MODULE, rebar_escript_plugin_main).
@@ -38,6 +39,32 @@
 %%%=============================================================================
 %%% API
 %%%=============================================================================
+
+-spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
+init(State) ->
+    Provider = providers:create([
+                                {name, ?PROVIDER},
+                                {module, ?MODULE},
+                                {bare, true},
+                                {deps, ?DEPS},
+                                {example, "rebar escriptize"},
+                                {opts, []},
+                                {short_desc, ""},
+                                {desc, ""}
+                                ]),
+    {ok, rebar_state:add_provider(State, Provider)}.
+
+-spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
+do(State) ->
+    rebar_log:log(info, "Building escript...", []),
+    [App] = rebar_state:project_apps(State),
+    AppFile = rebar_app_info:app_file(App),
+    run(State, AppFile),
+    {ok, State}.
+
+-spec format_error(any()) -> iolist().
+format_error(Reason) ->
+    io_lib:format("~p", [Reason]).
 
 %%------------------------------------------------------------------------------
 %% @doc
@@ -50,80 +77,33 @@
 %% the application.
 %% @end
 %%------------------------------------------------------------------------------
--spec post_compile(rebar_config:config(), file:filename()) -> ok.
-post_compile(Config, AppFile) ->
-    IsBaseDir = rebar_utils:processing_base_dir(Config),
-    IsAppDir = rebar_app_utils:is_app_dir(),
-    IsRelDir = rebar_rel_utils:is_rel_dir(),
-    case {IsBaseDir, IsAppDir, IsRelDir} of
-        {true, {true, AppFile}, false} ->
-            BaseDir = rebar_utils:base_dir(Config),
-            TempDir = temp_dir(BaseDir),
-            ok = rebar_utils:ensure_dir(filename:join([TempDir, "dummy"])),
-            ok = prepare_runner_module(TempDir),
-            AppsToStart = [AppName] = [app_name(Config, AppFile)],
-            AppFiles = [AppFile | app_files(dep_dirs(BaseDir, Config))],
-            PackagedApps = mk_links(TempDir, Config, AppFiles),
-            ok = prepare_main_module(TempDir, AppsToStart, PackagedApps),
-            Ez = create_ez(TempDir, Config, AppFile, PackagedApps),
-            ok = create_escript(Ez, BaseDir, Config, atom_to_list(AppName));
-        _ ->
-            ok
-    end.
-
-%%------------------------------------------------------------------------------
-%% @doc
-%% Create a standalone escript of a release in the release's base directory
-%% after generation with `rebar generate'.
-%% @end
-%%------------------------------------------------------------------------------
--spec post_generate(rebar_config:config(), file:filename()) -> ok.
-post_generate(Config, RelToolFile) ->
-    case rebar_rel_utils:is_rel_dir() of
-        {true, RelToolFile} ->
-            BaseDir = filename:dirname(RelToolFile),
-            RelToolConfig = reltool_cfg(Config, RelToolFile),
-            {RelFile, LibDir} = reltool_info(Config, RelToolConfig),
-            ok = prepare_runner_module(LibDir),
-            {RelName, AppsToStart} = rel_info(RelFile),
-            AppFiles = [AppFile | _] = app_files([LibDir]),
-            PackagedApps = [app_name(Config, AppF) || AppF <- AppFiles],
-            ok = prepare_main_module(LibDir, AppsToStart, PackagedApps),
-            Ez = create_ez(LibDir, Config, AppFile, PackagedApps),
-            ok = create_escript(Ez, BaseDir, Config, RelName);
-        _ ->
-            ok
-    end.
+-spec run(rebar_state:t(), file:filename()) -> ok.
+run(State, AppFile) ->
+    BaseDir = rebar_state:get(State, base_dir, "."),
+    TempDir = temp_dir(BaseDir),
+    ok = rebar_utils:ensure_dir(filename:join([TempDir, "dummy"])),
+    ok = prepare_runner_module(TempDir),
+    AppsToStart = [AppName] = [app_name(State, AppFile)],
+    AppFiles = [AppFile | app_files(dep_dirs(State))],
+    PackagedApps = mk_links(TempDir, State, AppFiles),
+    ok = prepare_main_module(TempDir, AppsToStart, PackagedApps),
+    Ez = create_ez(TempDir, State, AppFile, PackagedApps),
+    ok = create_escript(Ez, BaseDir, State, atom_to_list(AppName)).
 
 %%------------------------------------------------------------------------------
 %% @doc
 %% Remove the plugin's temporary directory and custom files after `rebar clean'.
 %% @end
 %%------------------------------------------------------------------------------
--spec post_clean(rebar_config:config(), file:filename()) -> ok.
-post_clean(Config, ResourceFile) ->
-    IsBaseDir = rebar_utils:processing_base_dir(Config),
-    IsAppDir = rebar_app_utils:is_app_dir(),
-    IsRelDir = rebar_rel_utils:is_rel_dir(),
-    case {IsBaseDir, IsAppDir, IsRelDir} of
-        {true, {true, AppFile}, false} when AppFile =:= ResourceFile ->
-            {OsFamily, _OsName} = os:type(),
-            BaseDir = rebar_utils:base_dir(Config),
-            rm_rf(temp_dir(BaseDir)),
-            AppName = atom_to_list(app_name(Config, AppFile)),
-            rm_rf(escript_path(OsFamily, BaseDir, AppName));
-        {_, false, {true, RelToolFile}} when RelToolFile =:= ResourceFile ->
-            {OsFamily, _OsName} = os:type(),
-            BaseDir = filename:dirname(RelToolFile),
-            RelToolConfig = reltool_cfg(Config, RelToolFile),
-            {RelFile, LibDir} = reltool_info(Config, RelToolConfig),
-            {RelName, _AppsToStart} = rel_info(RelFile),
-            rm_rf(main_path(LibDir, ?MAIN_MODULE)),
-            rm_rf(main_path(LibDir, rebar_escript_plugin_runner)),
-            rm_rf(escript_path(OsFamily, BaseDir, RelName));
-        _ ->
-            ok
-    end.
+-spec clean(rebar_state:t(), file:filename()) -> ok.
+clean(State, _ResourceFile) ->
+    [App] = rebar_state:project_apps(State),
+    AppFile = rebar_app_info:app_file(App),
+    {OsFamily, _OsName} = os:type(),
+    BaseDir = rebar_utils:base_dir(State),
+    rm_rf(temp_dir(BaseDir)),
+    AppName = atom_to_list(app_name(State, AppFile)),
+    rm_rf(escript_path(OsFamily, BaseDir, AppName)).
 
 %%%=============================================================================
 %%% Internal functions
@@ -147,21 +127,21 @@ app_files(DepDirs) ->
 %% of `.app' files in a temporary directory. This is needed to get the directory
 %% structure desired by the code server for the created `.ez' archive.
 %%------------------------------------------------------------------------------
-mk_links(TempDir, Config, AppFiles) ->
-    [mk_link(TempDir, Config, AppFile) || AppFile <- AppFiles].
+mk_links(TempDir, State, AppFiles) ->
+    [mk_link(TempDir, State, AppFile) || AppFile <- AppFiles].
 
 %%------------------------------------------------------------------------------
 %% @private
 %% Creates a link to the application directory corresponding to the given
 %% `.app' file.
 %%------------------------------------------------------------------------------
-mk_link(TempDir, Config, AppFile) ->
-    LinkName = app_link(TempDir, Config, AppFile),
+mk_link(TempDir, State, AppFile) ->
+    LinkName = app_link(TempDir, State, AppFile),
     case file:make_symlink(app_dir(AppFile), LinkName) of
         ok ->
-            app_name(Config, AppFile);
+            app_name(State, AppFile);
         {error, eexist} ->
-            app_name(Config, AppFile);
+            app_name(State, AppFile);
         Error ->
             Error
     end.
@@ -184,51 +164,28 @@ app_dir(AppFile) ->
 %%------------------------------------------------------------------------------
 %% @private
 %%------------------------------------------------------------------------------
-app_name(Config, AppFile) ->
-    element(2, rebar_app_utils:app_name(Config, AppFile)).
+app_name(State, AppFile) ->
+    element(2, rebar_app_utils:app_name(State, AppFile)).
 
 %%------------------------------------------------------------------------------
 %% @private
 %%------------------------------------------------------------------------------
-app_vsn(Config, AppFile) ->
-    element(2, rebar_app_utils:app_vsn(Config, AppFile)).
+app_vsn(State, AppFile) ->
+    element(2, rebar_app_utils:app_vsn(State, AppFile)).
 
 %%------------------------------------------------------------------------------
 %% @private
 %%------------------------------------------------------------------------------
-app_name_and_vsn(Config, AppFile) ->
-    AppName = atom_to_list(app_name(Config, AppFile)),
-    AppVsn = app_vsn(Config, AppFile),
+app_name_and_vsn(State, AppFile) ->
+    AppName = atom_to_list(app_name(State, AppFile)),
+    AppVsn = app_vsn(State, AppFile),
     AppName ++ "-" ++ AppVsn.
 
 %%------------------------------------------------------------------------------
 %% @private
 %%------------------------------------------------------------------------------
-app_link(TempDir, Config, AppFile) ->
-    filename:join([TempDir, app_name_and_vsn(Config, AppFile)]).
-
-%%------------------------------------------------------------------------------
-%% @private
-%%------------------------------------------------------------------------------
-reltool_cfg(Config, RelToolFile) ->
-    element(2, rebar_rel_utils:load_config(Config, RelToolFile)).
-
-%%------------------------------------------------------------------------------
-%% @private
-%% Return the paths to the releases `.rel' file and its `lib' directory.
-%%------------------------------------------------------------------------------
-reltool_info(Config, RelToolConfig) ->
-    {Name, Version} = rebar_rel_utils:get_reltool_release_info(RelToolConfig),
-    TargetDir = rebar_rel_utils:get_target_dir(Config, RelToolConfig),
-    {filename:join([TargetDir, "releases", Version, Name ++ ".rel"]),
-     filename:join([TargetDir, "lib"])}.
-
-%%------------------------------------------------------------------------------
-%% @private
-%%------------------------------------------------------------------------------
-rel_info(RelFile) ->
-    {ok, [{release, {RelName, _}, _, Apps}]} = file:consult(RelFile),
-    {RelName, [App || {App, _Version} <- Apps]}.
+app_link(TempDir, State, AppFile) ->
+    filename:join([TempDir, app_name_and_vsn(State, AppFile)]).
 
 %%------------------------------------------------------------------------------
 %% @private
@@ -241,10 +198,10 @@ temp_dir(BaseDir) -> filename:join([BaseDir, ?TEMP_DIR]).
 %% project/application. This includes the path configured as `deps_dir' and the
 %% paths specified as `lib_dirs' in the `rebar.config' file.
 %%------------------------------------------------------------------------------
-dep_dirs(BaseDir, Config) ->
-    {true, DepsDir} = rebar_deps:get_deps_dir(Config),
-    LibDirs = rebar_config:get_local(Config, lib_dirs, []),
-    [DepsDir | [filename:join([BaseDir, Dir]) || Dir <- LibDirs]].
+dep_dirs(State) ->
+    DepsDir = rebar_prv_install_deps:get_deps_dir(State),
+    LibDirs = rebar_state:get(State, lib_dirs, []),
+    [DepsDir | [filename:asbname(Dir) || Dir <- LibDirs]].
 
 %%------------------------------------------------------------------------------
 %% @private
@@ -264,12 +221,12 @@ dep_dirs(BaseDir, Config) ->
 %%  + ...
 %%
 %%------------------------------------------------------------------------------
-create_ez(TempDir, Config, AppFile, PackagedApps) ->
+create_ez(TempDir, State, AppFile, PackagedApps) ->
     Apps = string:join([atom_to_list(App) || App <- PackagedApps], ","),
     Paths = filename:join(["{" ++ Apps ++ "}-*", "{ebin,priv}"]),
     {ok, {_, Archive}} =
         zip:create(
-          app_name_and_vsn(Config, AppFile) ++ ".ez",
+          app_name_and_vsn(State, AppFile) ++ ".ez",
           filelib:wildcard(Paths, TempDir) ++
               filelib:wildcard("*.beam", TempDir),
           [{cwd, TempDir}, {uncompress, all}, memory]),
@@ -287,12 +244,12 @@ escript_path(OsFamily, BaseDir, EScript) ->
 %%------------------------------------------------------------------------------
 %% @private
 %%------------------------------------------------------------------------------
-create_escript(Ez, BaseDir, Config, EScript) ->
+create_escript(Ez, BaseDir, State, EScript) ->
     {OsFamily, _OsName} = os:type(),
     Path = escript_path(OsFamily, BaseDir, EScript),
     HeartArg = "-heart",
     MainArg = "-escript main " ++ atom_to_list(?MAIN_MODULE),
-    EmuArgs = string:join([HeartArg, MainArg, get_emu_args(Config)], " "),
+    EmuArgs = string:join([HeartArg, MainArg, get_emu_args(State)], " "),
     Sections = [shebang, comment, {emu_args, EmuArgs}, {archive, Ez}],
     ok = escript:create(Path, Sections),
     ok = set_executable(OsFamily, Path).
@@ -368,8 +325,8 @@ prepare_main_module(TempDir, AppsToStart, PackagedApps) ->
 %% correctly. Additionally, the `-heart' parameter is always set (for cleanup)
 %% and it is therefore not necessary to specify it.
 %%------------------------------------------------------------------------------
-get_emu_args(Config) ->
-    Cfg = rebar_config:get(Config, ?MODULE, []),
+get_emu_args(State) ->
+    Cfg = rebar_state:get(State, ?MODULE, []),
     case proplists:get_value(emu_args, Cfg, "") of
         EmuArgs when is_list(EmuArgs) -> EmuArgs
     end.
